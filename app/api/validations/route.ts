ffd8b0188fd66dafe7ce19d1772a1f28f566d045
Entropyi <jemo1x503@gmail.com>
2024-08-13 10:48:04 +0300

    api changes

--- app/api/validations/route.ts
+++ app/api/validations/route.ts
@@ -1,10 +1,31 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import {NextRequest, NextResponse} from "next/server";
 import {PrismaClient} from '@prisma/client'
+import * as v from "valibot";
 
-const prisma = new PrismaClient()
+
+export async function GET(req: NextRequest, res: NextResponse) {
+    const reqUrl = new URL(req.url);
+
+    const paramsSchema = v.object({
+        email: v.pipe(v.string(), v.email(), v.nonEmpty()),
+        phoneNumber: v.pipe(v.string(), v.maxLength(10), v.minLength(10), v.nonEmpty())
+    })
+
+    try{
+        const paramsDara = v.parse(
+            paramsSchema,
+            {
+                email: reqUrl.searchParams.get("email"),
+                phoneNumber: reqUrl.searchParams.get("number"),
+            }
+        )
+
+        return NextResponse.json({success: true, email: paramsDara.email});
+
+    } catch (e){
+        return NextResponse.json({success: false, err:e.[0].kind} , {status:500});
+
+    }
 
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    const { email, number } = req.query;
-    res.status(200).json({ number: number, email: email });
 }
